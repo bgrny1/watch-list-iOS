30de3823b475f4aa59f14240b29ebf7517f87279
bgrny1 <girenaybuket@gmail.com>
2023-11-24 23:14:13 +0300

    Initial Commit

---
+++ watchList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  watchList
+//
+//  Created by Buket girenay on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
